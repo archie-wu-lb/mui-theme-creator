3f81176e0ff75690e92989706bfe7b936b1cb5d8
archie-wu-lb <archie.wu@getoken.io>
2023-02-10 11:37:58 +0800

    fix button margin

--- src/components/MuiComponentSamples/Samples/Button.tsx
+++ src/components/MuiComponentSamples/Samples/Button.tsx
@@ -2,16 +2,16 @@ import CloudUploadIcon from "@mui/icons-material/CloudUpload"
 import DeleteIcon from "@mui/icons-material/Delete"
 import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice"
 import SaveIcon from "@mui/icons-material/Save"
-import { Box, Typography } from "@mui/material"
+import { Box, styled, Theme, Typography } from "@mui/material"
 import Button from "@mui/material/Button"
 import React from "react"
 
-const groupStyle = {
-  mb: 2,
+const ButtonGroup = styled(Box)(({ theme }: { theme: Theme }) => ({
+  marginBottom: 2,
   "& > *": {
-    m: 1,
+    margin: `${theme.spacing(1)} !important`,
   },
-}
+}))
 
 export default function ButtonsExample() {
   return (
@@ -19,8 +19,10 @@ export default function ButtonsExample() {
       <Typography variant="h6" id="contained-buttons">
         Contained Buttons
       </Typography>
-      <Box sx={groupStyle}>
-        <Button variant="contained">Default</Button>
+      <ButtonGroup>
+        <Button variant="contained" color="inherit">
+          Default
+        </Button>
         <Button variant="contained" color="primary">
           Primary
         </Button>
@@ -33,25 +35,43 @@ export default function ButtonsExample() {
         <Button variant="contained" color="primary" href="#contained-buttons">
           Link
         </Button>
-      </Box>
+      </ButtonGroup>
+
+      <Typography variant="h6" id="contained-buttons">
+        Sizes
+      </Typography>
+      <ButtonGroup>
+        <Button variant="contained" color="primary" size="small">
+          Small
+        </Button>
+        <Button variant="contained" color="primary" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" color="primary" size="large">
+          Large
+        </Button>
+        <Button variant="contained" color="primary" size="large" fullWidth>
+          FullWidth
+        </Button>
+      </ButtonGroup>
 
       <Typography variant="h6" id="text-buttons">
         Text Buttons
       </Typography>
-      <Box sx={groupStyle}>
-        <Button>Default</Button>
+      <ButtonGroup>
+        <Button color="inherit">Default</Button>
         <Button color="primary">Primary</Button>
         <Button color="secondary">Secondary</Button>
         <Button disabled>Disabled</Button>
         <Button color="primary" href="#text-buttons">
           Link
         </Button>
-      </Box>
+      </ButtonGroup>
 
       <Typography variant="h6" id="outlined-buttons">
         Outlined Buttons
       </Typography>
-      <Box sx={groupStyle}>
+      <ButtonGroup>
         <Button variant="outlined">Default</Button>
         <Button variant="outlined" color="primary">
           Primary
@@ -65,12 +85,12 @@ export default function ButtonsExample() {
         <Button variant="outlined" color="primary" href="#outlined-buttons">
           Link
         </Button>
-      </Box>
+      </ButtonGroup>
 
       <Typography variant="h6" id="buttons-with-icons">
         Buttons with icons and label
       </Typography>
-      <Box sx={groupStyle}>
+      <ButtonGroup>
         <Button
           variant="contained"
           color="secondary"
@@ -89,23 +109,10 @@ export default function ButtonsExample() {
         >
           Talk
         </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          size="small"
-          startIcon={<SaveIcon />}
-        >
+        <Button variant="contained" color="primary" startIcon={<SaveIcon />}>
           Save
         </Button>
-        <Button
-          variant="contained"
-          color="primary"
-          size="large"
-          startIcon={<SaveIcon />}
-        >
-          Save
-        </Button>
-      </Box>
+      </ButtonGroup>
     </div>
   )
 }
